5e4bf8186d5e8179b2d94158024c715c0b10a443
FarhanKurnia <itsfarhankurnia@gmail.com>
2022-03-29 20:55:45 +0700

    Add penjelasan

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'HomePage.dart';
 void main() {
   runApp(MyApp());
 }
-
+//Statelesswidget digunakan untuk menampilkan data-data yang bersifat statis (tidak mengalami perubahan)
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
+//Statefulwidget digunakan untuk menampilkan data-data yang bersifat dinamis (dapat mengalami perubahan)
 class LoginDemo extends StatefulWidget {
   @override
   _LoginDemoState createState() => _LoginDemoState();
@@ -23,21 +23,28 @@ class LoginDemo extends StatefulWidget {
 class _LoginDemoState extends State<LoginDemo> {
   @override
   Widget build(BuildContext context) {
+    //Widget Scaffold digunakan sebagai widget utama untuk membuat sebuah halaman 
     return Scaffold(
-      //Set Warna Backgrounnd putih
+      //backgorunfColor digunakan untuk membuat Warna Backgrounnd putih
       backgroundColor: Colors.white,
+      //Widget AppBar digunakan untuk digunakan sebagai bagian header yang dapat memuat judul ataupun navigasi dari aplikasi
       appBar: AppBar(
-        //Tittle halaman login
+          //background color app bar
           backgroundColor: Colors.lightBlueAccent,
+           //Tittle halaman login
         title: Text("Login Page"),
       ),
       body: SingleChildScrollView(
+        //widget column mengatur tata letak atau posisi widget secara vertikal (dari atas ke bawah)
         child: Column(
           children: <Widget>[
+            //digunakan untuk memberikan Padding
             Padding(
               //Padding atas dan bawah dari image
               padding: const EdgeInsets.only(top: 60.0, bottom: 30.0),
+              //Widget: child center digunakan untuk membuat posisi widget berada di tengah
               child: Center(
+                //Widget: Child Container digunakan untuk membungkus atribut width, height dan widget image
                 child: Container(
                     width: 200,
                     height: 150,
@@ -45,6 +52,7 @@ class _LoginDemoState extends State<LoginDemo> {
                     child: Image.asset('asset/images/flutter-logo-bird.png')),
               ),
             ),
+            //digunakan untuk memberikan Padding
             Padding(
               //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
               padding: EdgeInsets.symmetric(horizontal: 15),
@@ -56,11 +64,12 @@ class _LoginDemoState extends State<LoginDemo> {
                     hintText: 'Enter valid E-mail as Farhan@gmail.com'),
               ),
             ),
+            //digunakan untuk memberikan Padding
             Padding(
               padding: const EdgeInsets.only(
                   left: 15.0, right: 15.0, top: 15, bottom: 0),
               //padding: EdgeInsets.symmetric(horizontal: 15),
-              //Widget: child text untuk Textfield Password
+              //Widget: child Textfield Password agar user dapat melakukan input password
               child: TextField(
                 obscureText: true,
                 decoration: InputDecoration(
@@ -69,9 +78,11 @@ class _LoginDemoState extends State<LoginDemo> {
                     hintText: 'Enter secure password'),
               ),
             ),
+            //Widget flat button digunakan untuk membuat tombol forgot password tanpa efek-efek seperti bayangan dan lain-lain.
             FlatButton(
+              // OnPressed disini kosong, dapat diisi dengan mengarahkan ke halaman forgit password. 
               onPressed: (){
-                //TODO FORGOT PASSWORD SCREEN GOES HERE
+                //bagian koding jika tombol ditekan
               },
               //Widget: child text untuk Flatbutton hyperlink lupa password
               child: Text(
@@ -79,32 +90,38 @@ class _LoginDemoState extends State<LoginDemo> {
                 style: TextStyle(color: Colors.black12, fontSize: 15),
               ),
             ),
+            //Widget: Container digunakan untuk membungkus atribut width, height, decoration, color, flatbutton dan text login
             Container(
               height: 50,
               width: 150,
               decoration: BoxDecoration(
                   color: Colors.lightBlueAccent, borderRadius: BorderRadius.circular(20)),
-              //Widget: child untuk Flatbutton Login
+              //Widget flat button digunakan untuk membuat tombol tanpa efek-efek seperti bayangan dan lain-lain.
               child: FlatButton(
+                // Onpressed disini ketika tombol diklik maka akan diarahkan ke halaman home page
                 onPressed: () {
+                  //Navigator push digunakan untuk membuka halaman baru
                   Navigator.push(
                       context, MaterialPageRoute(builder: (_) => HomePage()));
                 },
-                //Widget: child untuk text Login dalam button
+                //Widget: child untuk text Login dalam flatbutton
                 child: Text(
                   'Login',
                   style: TextStyle(color: Colors.white, fontSize: 20),
                 ),
               ),
             ),
+            //Widget untuk mengatur jarak dengan atribut tinggi 40
             SizedBox(
               height: 40,
             ),
+            // //Widget flat button digunakan untuk membuat tombol tanpa efek-efek seperti bayangan dan lain-lain.
             FlatButton(
+              // OnPressed disini kosong, dapat diisi dengan mengarahkan ke halaman register. 
               onPressed: (){
-                //TODO FORGOT PASSWORD SCREEN GOES HERE
+                //bagian koding jika tombol ditekan
               },
-              //Widget: child untuk text flatbutton hyperlink Create Account
+              //Widget: child untuk text flatbutton hyperlink register atau Create Account
               child: Text(
                 'New User? Create Account',
                 style: TextStyle(color: Colors.black12, fontSize: 15),
