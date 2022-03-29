05f41fb754d977ecc1443fa0ba174a73dd4532f8
FarhanKurnia <itsfarhankurnia@gmail.com>
2022-03-29 21:10:26 +0700

    Add penjelasan

--- lib/main.dart
+++ lib/main.dart
@@ -56,7 +56,7 @@ class _LoginDemoState extends State<LoginDemo> {
             Padding(
               //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
               padding: EdgeInsets.symmetric(horizontal: 15),
-              //Widget: child untuk Textfield E-Mail
+              //Widget: child Textfield Email agar user dapat melakukan input email
               child: TextField(
                 decoration: InputDecoration(
                     border: OutlineInputBorder(),
