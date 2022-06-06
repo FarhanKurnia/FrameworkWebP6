d9d3cc277862c7935d1ecc9d0ee8e3884967d994
FarhanKurnia <itsfarhankurnia@gmail.com>
2022-06-07 00:47:53 +0700

    Add Firebase Sync

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'HomePage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+
 
 void main() {
   runApp(MyApp());
+
 }
 //Statelesswidget digunakan untuk menampilkan data-data yang bersifat statis (tidak mengalami perubahan)
 class MyApp extends StatelessWidget {
